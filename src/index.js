ecff9111a94337e592bbaaa19dc3875756b43240
John <wangjiarui@mudu.tv>
2019-03-10 23:02:18 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import './index.css';
-import App from './App';
+import App from './entry';
 import store from './store'
 import * as serviceWorker from './serviceWorker';
 
